9c3a6e350b649ea0b20ea06d0a8fe4388a4e3cfd
yash-251198 <shetyeyash444@gmail.com>
2025-09-05 00:57:55 +0530

    reusable logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import logo from "/logo.png";
+
+const Logo = () => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="items-center space-x-1 cursor-pointer flex justify-center md:justify-start p-4"
+      onClick={() => navigate("/dashboard")}
+    >
+      <div className="w-12 h-12 rounded-full flex items-center justify-center">
+        <img src={logo} alt="logo" />
+      </div>
+      <span className="font-bold text-2xl text-gray-800 hidden md:block">
+        Grabit
+      </span>
+    </div>
+  );
+};
+
+export default Logo;

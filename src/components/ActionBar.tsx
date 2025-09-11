1e94ba08323bb839aa04d2b0b20275f5d4d5d1ec
yash-251198 <shetyeyash444@gmail.com>
2025-09-11 20:53:05 +0530

    removed prop drilling

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,5 +1,6 @@
-import Service from "./Service";
 import type { service, vendorservices } from "@/types/types";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 interface ActionBarProps {
   setOpenService: (service: vendorservices) => void;
@@ -11,12 +12,14 @@ const ActionBar = ({ setOpenService, openService, links }: ActionBarProps) => {
   return (
     <div className="gap-3 grid grid-flow-col max-w-sm">
       {links.map((service, index) => (
-        <Service
+        <Button
           key={index}
-          service={service}
-          setOpen={setOpenService}
-          open={openService === service.serviceName}
-        />
+          variant={openService === service.serviceName ? "primary" : "outline"}
+          className={cn("w-35 p-2 flex justify-center gap-2 border rounded-sm ")}
+          onClick={() => setOpenService(service.serviceName)}
+        >
+          <span>{service.text}</span>
+        </Button>
       ))}
     </div>
   );

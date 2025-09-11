9ba4939f0215aadcb7dbf129375d78fedde27609
yash-251198 <shetyeyash444@gmail.com>
2025-09-11 20:56:44 +0530

    creted globlal state and state management function for editing product

--- src/store/slices/vendorslices.ts
+++ src/store/slices/vendorslices.ts
@@ -1,13 +1,22 @@
-import type { ProductType } from "@/types/formType";
 import type { Vendor, VendorSlices } from "src/types/vendor";
 import type { StateCreator } from "zustand";
 
-export const vendorSlices: StateCreator<VendorSlices> = (set) => ({
+export const vendorSlices: StateCreator<VendorSlices> = (set, get) => ({
   vendor: null,
   setVendor: (vendor: Vendor) => set({ vendor }),
+  products: [],
+  setProducts: (products) => set({ products: [...products] }),
   editProduct: null,
-  setEditProduct: (product: ProductType) => {
-    set({ editProduct: product });
+  setEditProduct: () => {
+    const editProduct = get().products.find(
+      (p) => p._id === get().selectedProducts[0]
+    );
+    set({ editProduct });
+  },
+  selectedProducts: [],
+  addSelectedProducts: (product) => {
+    const selectedProducts = [...get().selectedProducts, product];
+    set({ selectedProducts });
   },
   logout: () =>
     set({

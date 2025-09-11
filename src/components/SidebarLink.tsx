ed473f70578a7b4864f2c78e902408250e588aed
yash-251198 <shetyeyash444@gmail.com>
2025-09-11 20:53:57 +0530

    optimized sidebar link to accept props and make it actul reusable component

--- src/components/SidebarLink.tsx
+++ src/components/SidebarLink.tsx
@@ -5,17 +5,21 @@ import { Link } from "react-router-dom";
 const SidebarLink = ({
   link,
   active,
+  ...props
 }: {
   link: sidebarlink;
   active: boolean;
+  [key: string]: unknown;
 }) => {
   return (
     <Link
       title={link.text}
       to={`${link.navigate}`}
       className={cn("flex gap-2 p-2 rounded-sm justify-start w-full", {
-        "bg-primary": active,
+        "bg-gradient-to-r from-amber-500 to-amber-600 hover:from-amber-600 hover:to-amber-700 text-white shadow-md hover:shadow-lg":
+          active,
       })}
+      {...props}
     >
       <link.icon className="" />
       <span className="font-medium">{link.text}</span>
